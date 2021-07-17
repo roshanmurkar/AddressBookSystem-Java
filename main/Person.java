233cdfd543e8e3a9f63de2744cc6298afe5b81ce
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-17 12:28:25 +0530

    define private variables

--- main/Person.java
+++ main/Person.java
@@ -1,5 +1,12 @@
 package main;
 
 public class Person {
-
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String cityName;
+	private String stateName;
+	private long zipCode;
+	private long phoneNumber;
+	private String emailAddress;
 }
