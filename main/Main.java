89499132e3ad7d40afe8d3d05cde3168a2cbf64c
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-17 12:44:29 +0530

    adding data of one person through main

--- main/Main.java
+++ main/Main.java
@@ -4,7 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Person person = new Person();
+		int i = 1;
+		person.setFirstName("roshan");
+		person.setLastName("Murkar");
+		person.setAddress("at post sawantwadi sindhudurg");
+		person.setCityName("sawaantwadi");
+		person.setStateName("maharastra");
+		person.setZipCode("416510");
+		person.setPhoneNumber("1234567890");
+		person.setEmailAddress("host@gmail.com");
+		
+		
+		//printing all info
+		System.out.println("first Name :-" + person.getFirstName() 
+							+ " \nLast Name :-" + person.getLastName()
+							+ " \naddress :-" + person.getAddress() 
+							+ " \ncity Name :-" + person.getCityName() 
+							+ "\nstate Name :-"+ person.getStateName() 
+							+ " \nzip :-" + person.getZipCode() + " "
+							+ "\nphone :-" + person.getPhoneNumber()
+							+ " \nemail :-" + person.getEmailAddress());
 	}
 
 }
--- main/Person.java
+++ main/Person.java
@@ -1,12 +1,114 @@
 package main;
 
 public class Person {
+	//fields of address book
 	private String firstName;
 	private String lastName;
 	private String address;
 	private String cityName;
 	private String stateName;
-	private long zipCode;
-	private long phoneNumber;
+	private String zipCode;
+	private String phoneNumber;
 	private String emailAddress;
+	
+	//use getter setter
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the cityName
+	 */
+	public String getCityName() {
+		return cityName;
+	}
+	/**
+	 * @param cityName the cityName to set
+	 */
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	/**
+	 * @return the stateName
+	 */
+	public String getStateName() {
+		return stateName;
+	}
+	/**
+	 * @param stateName the stateName to set
+	 */
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	/**
+	 * @return the zipCode
+	 */
+	public String getZipCode() {
+		return zipCode;
+	}
+	/**
+	 * @param args the zipCode to set
+	 */
+	public void setZipCode(String args) {
+		this.zipCode = args;
+	}
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * @param args the phoneNumber to set
+	 */
+	public void setPhoneNumber(String args) {
+		this.phoneNumber = args;
+	}
+	/**
+	 * @return the emailAddress
+	 */
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	/**
+	 * @param emailAddress the emailAddress to set
+	 */
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	
+	
 }
